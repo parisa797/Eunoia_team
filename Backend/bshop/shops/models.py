f319d9e71ef5778ca60ae8d62978d8ac52ea1ebb
zahra <zmahmoudzadeh78@gmail.com>
2021-03-29 17:02:52 +0430

    Add shops models

--- Backend/bshop/shops/models.py
+++ Backend/bshop/shops/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.apps import apps
 
-# Create your models here.
+class Shop(models.Model):
+
+	title = models.CharField(max_length = 100, blank=False)
+    user = models.ForeignKey(User,related_name='users',on_delete=models.CASCADE) 
+	logo = models.ImageField(blank=True , upload_to='image/')
+	address = models.CharField(max_length = 500, blank=False)
+    theme = models.IntegerField()
+    shomare_sabt = models.IntegerField()
+
+	def str(self):
+		return self.title
