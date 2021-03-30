d58135622b54b6418595ceb5bcbc1ff507a2acca
unknown <parisa78alaie@gmail.com>
2021-03-30 12:19:29 +0430

    add profile. sedit profile and delete profile and delete picture

--- Backend/bshop/bshop/urls.py
+++ Backend/bshop/bshop/urls.py
@@ -1,4 +1,4 @@
-"""bshop URL Configuration
+"""BShop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^account/', include('allauth.urls')),
+    path('', include('users.urls')),
+
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
