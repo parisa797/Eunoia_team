7415d71b058f9a990f3fd0be758e9397481e3195
unknown <parisa78alaie@gmail.com>
2021-03-30 12:02:31 +0430

    add login and sign up

--- Backend/bshop/bshop/asgi.py
+++ Backend/bshop/bshop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bshop project.
+ASGI config for BShop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BShop.settings')
 
 application = get_asgi_application()
