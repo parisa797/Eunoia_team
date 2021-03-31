2bb1bbb765c83379593fecdcf58355ab7bea8f68
zahra <zmahmoudzadeh78@gmail.com>
2021-03-31 18:56:13 +0430

    Filter shop with user

--- Backend/bshop/shops/urls.py
+++ Backend/bshop/shops/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     
     path('shops/', views.ShopListCreateAPIView.as_view()),
-    path('shops/<int:pk>', views.ShopRetrieveUpdateDestroyAPIView.as_view())
+    path('shops/<int:pk>', views.ShopRetrieveUpdateDestroyAPIView.as_view()),
+    path('user/', views.ShopListAPIView.as_view()),
 ]
--- Backend/bshop/shops/views.py
+++ Backend/bshop/shops/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
+from rest_framework import filters
 
 
 from .models import Shop
@@ -22,6 +23,19 @@ class ShopListCreateAPIView(generics.ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class OwnerFilterBackend(filters.BaseFilterBackend):
+        def filter_queryset(self, request, queryset, view):
+            return queryset.filter(user=request.user)
+
+
+class ShopListAPIView(generics.ListAPIView):
+    
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+    filter_backends = (OwnerFilterBackend,)
 
 
 
