7c1a7863de1b2bdc4184d04034abca085699e534
unknown <parisa78alaie@gmail.com>
2021-04-02 19:40:33 +0430

    add test for login and logout and sign up

--- Backend/bshop/bshop/urls.py
+++ Backend/bshop/bshop/urls.py
@@ -1,18 +1,3 @@
-"""BShop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
@@ -25,5 +10,5 @@ urlpatterns = [
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
     path('', include('users.urls')),
-
+    path('api/v1/shops/', include('shops.urls')),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
---
+++ Backend/bshop/shops/views.py
@@ -0,0 +1,45 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
+from rest_framework import filters
+
+
+from .models import Shop
+from .serializers import ShopSerializer
+from users.models import MyUser
+
+class ShopListCreateAPIView(generics.ListCreateAPIView):
+    
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer_data = request.data.copy()
+        serializer_data.update({'user':request.user.id})
+        serializer = self.get_serializer(data=serializer_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class OwnerFilterBackend(filters.BaseFilterBackend):
+        def filter_queryset(self, request, queryset, view):
+            return queryset.filter(user=request.user)
+
+
+class ShopListAPIView(generics.ListAPIView):
+    
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+    filter_backends = (OwnerFilterBackend,)
+
+
+
+class ShopRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
