0d3e52fa8e272c35ba01aa6d0f7525e41d22ce41
ShMob <mobashers313@gmail.com>
2021-04-20 21:19:48 +0430

    FIX homepage and landing page and Navbar UI design, along with loading page before homepage and footer

---
+++ Frontend/Website/bshop/src/PageFooter.js
@@ -0,0 +1,27 @@
+import './PageFooter.css'
+
+function PageFooter(props) {
+    return (<div className="page-footer">
+        <div className="row" >
+            <div className="col-12 col-sm-6 col-md-4" >
+                <h4>بیشتر بدانید</h4>
+                <a>درباره ما</a>
+                <a>اپلیکیشن اندروید</a>
+                <a>قوانین سایت</a>
+            </div>
+            <div className="col-12 col-sm-6 col-md-4" >
+                <h4>ارتباط با ما</h4>
+                <a>تماس با ما</a>
+                <a>حریم خصوصی</a>
+            </div>
+            <div className="col-12 col-sm-6 col-md-4" >
+                <h4>به ما بپیوندید!</h4>
+                <a>ثبت فروشگاه</a>
+                <a>قوانین ثبت فروشگاه</a>
+            </div>
+        </div>
+        <p>BShop 2021</p>
+    </div>)
+}
+
+export default PageFooter;
