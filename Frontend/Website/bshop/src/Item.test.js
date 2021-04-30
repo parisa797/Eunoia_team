22408e2a853d12af5680a28bf71e792c4feb7a4b
fateme jouyande <fj.1403@yahoo.com>
2021-04-30 21:32:30 +0430

    test login and sign up and item

--- Frontend/Website/bshop/src/Item.test.js
+++ Frontend/Website/bshop/src/Item.test.js
@@ -29,48 +29,19 @@ afterEach(() => {
 });
 
 
-// test("titleItem input in all ways", async () => {
-//     localStorage.setItem('username', 'abc')
-//     jest.spyOn(global, "fetch").mockImplementation(() =>
-//       Promise.resolve({
-//         status: 200
-//       })
-// );
-//     var page;
-//     var name;
-//     await act(async () => {
-//     page = await render(<Item />);
-//     name = await page.getByTestId("item");
-//     });
-//     await act(async () => {
-//         await name.focus();
-//         expect(name).toHaveValue("");
-//         await fireEvent.change(name, { target: { value: 'a' } });
-//         expect(name).toHaveValue("a");
-//         await fireEvent.change(name, { target: { value: 'ab' } });
-//         expect(name).toHaveValue("ab");
-//         await fireEvent.change(name, { target: { value: 'lsjfaiejfailejiajfeijflaeij' } });
-//         expect(name).toHaveValue('lsjfaiejfailejiajfeijflaeij');
-//         await fireEvent.change(name, { target: { value: '' } });
-//         expect(name).toHaveValue('');
-//       })
-//       });
 
-    test("descriptionItem input in all ways", async () => {
+    test("name and description Item test", async () => {
     localStorage.setItem('username', 'abc')
-    //Teste item ye farghi ba testaie ghablit dare, oonam inke avalesh gharare ba ye fetch zadan etelaate iteme ro begire mage na?
-    //pas inja baraye fetchesh bayad ye body ham bezarim va ye iteme alaki dorost konim bedim behesh.
-    //hamintor ke tooie payamaye skype goftam gharare id e shop va item ro baraye fetch kardan az window.location.pathname begire. pas bayad oon window.location.pathname ro joori konim ke in fetch ro betone anjam bede va toie Item.js error nade ke window.location nadari:
     global.window = Object.create(window);
     const url = "/store/1/item/1";
     Object.defineProperty(window, 'location', {
         value: {
             href: url,
-            pathname: url //in url ro gozashtim toie window.location.pathname ke toie Item.js azash estefade kone
+            pathname: url 
         }
     });
 
-    //ye item misazim ke toie code Item.js in item ro az fetch begire va jash bezare
+   
     const fakeItem = {
         name:"itemmm",
         Expiration_Date: "1400-03-03",
@@ -82,29 +53,161 @@ afterEach(() => {
     jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
         status: 200,
-        json: () => fakeItem //hala fetchemon begheir az status ye json ham pas mide ke tosh fakeItemie ke sakhtim
+        json: () => fakeItem 
         })
 );
-var page;
-var description;
-await act(async () => {
-    page = await render(<Item />); //be jaie Item neveshte boodi item4 bara hamin asan componente drosti ro render nemikard
-    description = await page.getByTestId("item");
-});
-expect(page.queryByTestId("item-name")).not.toBeNull();
-expect(page.queryByTestId("item-description")).toBeNull();
-expect(page.queryByTestId("item-name")).toHaveTextContent(fakeItem.name);
+        var page;
+        var description;
+        await act(async () => {
+            page = await render(<Item />); 
+            description = await page.getByTestId("item");
+        });
+        expect(page.queryByTestId("item-name")).not.toBeNull();
+        expect(page.queryByTestId("item-description")).toBeNull();
 
-    // await act(async () => {
-    //     await description.focus();
-    //     expect(description).toHaveValue("");
-    //     await fireEvent.change(description, { target: { value: 'ماست' } });
-    //     expect(description).toHaveValue("ماست");
-    //     await fireEvent.change(description, { target: { value: 'sm' } });
-    //     expect(description).toHaveValue("sm");
-    //     await fireEvent.change(description, { target: { value: 'lsjfaiejfailejiajfeijflaeij' } });
-    //     expect(description).toHaveValue('lsjfaiejfailejiajfeijflaeij');
-    //     await fireEvent.change(description, { target: { value: '' } });
-    //     expect(description).toHaveValue('');
-    // })
     });
+
+    test(" have description Item test", async () => {
+        localStorage.setItem('username', 'abc')
+        global.window = Object.create(window);
+        const url = "/store/1/item/1";
+        Object.defineProperty(window, 'location', {
+            value: {
+                href: url,
+                pathname: url 
+            }
+        });
+    
+        const fakeItem = {
+            name:"itemmm",
+            Expiration_jalali: "1400-03-03",
+            manufacture_jalali: "1400-02-13",
+            price: "90000",
+            discount: "5",
+            count: 30,
+            description:"khoshmaze"
+     }
+        jest.spyOn(global, "fetch").mockImplementation(() =>
+            Promise.resolve({
+            status: 200,
+            json: () => fakeItem 
+            })
+    );
+            var page;
+            var description;
+            await act(async () => {
+                page = await render(<Item />); 
+                description = await page.getByTestId("item");
+            });
+            expect(page.queryByTestId("item-description")).not.toBeNull();
+            expect(page.queryByTestId("item-name")).toHaveTextContent(fakeItem.name);
+
+        });
+        
+        test(" have not date Item test", async () => {
+            localStorage.setItem('username', 'abc')
+            global.window = Object.create(window);
+            const url = "/store/1/item/1";
+            Object.defineProperty(window, 'location', {
+                value: {
+                    href: url,
+                    pathname: url 
+                }
+            });
+
+            const fakeItem = {
+                name:"itemmm",
+                price: "90000",
+                discount: "5",
+                count: 30,
+                description:"khoshmaze"
+         }
+            jest.spyOn(global, "fetch").mockImplementation(() =>
+                Promise.resolve({
+                status: 200,
+                json: () => fakeItem 
+                })
+        );
+        var page;
+        var description;
+        await act(async () => {
+            page = await render(<Item />); 
+            description = await page.getByTestId("item");
+        });
+        expect(page.queryByTestId("item-manufacture_jalali")).toBeNull();
+        expect(page.queryByTestId("item-Expiration_jalali")).toBeNull();
+    
+            });
+
+        
+        test(" have text Item test", async () => {
+            localStorage.setItem('username', 'abc')
+            global.window = Object.create(window);
+            const url = "/store/1/item/1";
+            Object.defineProperty(window, 'location', {
+                value: {
+                    href: url,
+                    pathname: url 
+                }
+            });
+
+            const fakeItem = {
+                name:"itemmm",
+                price: "90000",
+                discount: "5",
+                count: 30,
+                description:"khoshmaze"
+         }
+            jest.spyOn(global, "fetch").mockImplementation(() =>
+                Promise.resolve({
+                status: 200,
+                json: () => fakeItem 
+                })
+        );
+        var page;
+        await act(async () => {
+            page = await render(<Item />); 
+
+        });
+
+        expect(page.queryByTestId("item-name")).toHaveTextContent(fakeItem.name);
+        expect(page.queryByTestId("item")).toHaveTextContent(fakeItem.description);
+            });
+
+
+            test(" have date Item test", async () => {
+                localStorage.setItem('username', 'abc')
+                global.window = Object.create(window);
+                const url = "/store/1/item/1";
+                Object.defineProperty(window, 'location', {
+                    value: {
+                        href: url,
+                        pathname: url 
+                    }
+                });
+    
+                const fakeItem = {
+                    name:"itemmm",
+                    Expiration_jalali: "1400-03-03",
+                    manufacture_jalali: "1400-02-13",
+                    price: "90000",
+                    discount: "5",
+                    count: 30,
+                    description:"khoshmaze"
+             }
+                jest.spyOn(global, "fetch").mockImplementation(() =>
+                    Promise.resolve({
+                    status: 200,
+                    json: () => fakeItem 
+                    })
+            );
+            var page;
+            var description;
+            await act(async () => {
+                page = await render(<Item />); 
+                description = await page.getByTestId("item");
+            });
+            expect(page.queryByTestId("item-manufacture_jalali")).not.toBeNull();
+            expect(page.queryByTestId("item-Expiration_jalali")).not.toBeNull();
+        
+                });
--- Frontend/Website/bshop/src/register.test.js
+++ Frontend/Website/bshop/src/register.test.js
@@ -4,7 +4,13 @@ import { act } from "react-dom/test-utils";
 import Register from './register';
 import '@testing-library/jest-dom';
 
-
+const register = () => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+    email: ""
+  });
+}
 
 let container = null;
 beforeEach(() => {
@@ -22,18 +28,98 @@ afterEach(() => {
 
 
 test("register users", async () => {
-    // const Register = {
-    //     email: "fj.1403@yahoo.com",
-    //     password: "12345",
-    //     username: "setare1"
-    // };
+    const Register = {
+        email: "fj.1403@yahoo.com",
+        password: "12345",
+        username: "setare1"
+    };
     jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
-        json: () => Promise.resolve({})
+        json: () => Promise.resolve(Register)
         })
     );
     var page;
     await act(async () => {
-        page = await render(<Register />);
+        page = await render(<register />);
     });
+});
+
+
+test("register users by username", async () => {
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+      json: () => Promise.resolve(Register)
+      })
+  );
+  var page;
+  var username;
+  await act(async () => {
+      page = await render(<Register />);
+      username = await page.getByTestId("register-username")
+  });
+  await act(async () => {
+    await username.focus();
+    expect(username).toHaveValue("");
+    await fireEvent.change(username, { target: { value: 'a' } });
+    expect(username).toHaveValue("a");
+    await fireEvent.change(username, { target: { value: 'ab' } });
+    expect(username).toHaveValue("ab");
+    await fireEvent.change(username, { target: { value: 'lsjfaie' } });
+    expect(username).toHaveValue('lsjfaie');
+    await fireEvent.change(username, { target: { value: '' } });
+    expect(username).toHaveValue('');
+  })
+});
+
+test("register users by email", async () => {
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+      json: () => Promise.resolve(Register)
+      })
+  );
+  var page;
+  var email;
+  await act(async () => {
+      page = await render(<Register />);
+      email = await page.getByTestId("register-email")
+  });
+  await act(async () => {
+    await email.focus();
+    expect(email).toHaveValue("");
+    await fireEvent.change(email, { target: { value: 'a' } });
+    expect(email).toHaveValue("a");
+    await fireEvent.change(email, { target: { value: 'ab' } });
+    expect(email).toHaveValue("ab");
+    await fireEvent.change(email, { target: { value: 'fj.1403@gmail.com' } });
+    expect(email).toHaveValue('fj.1403@gmail.com');
+    await fireEvent.change(email, { target: { value: '' } });
+    expect(email).toHaveValue('');
+  })
+});
+
+
+test("register users password", async () => {
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+      json: () => Promise.resolve(Register)
+      })
+  );
+  var page;
+  var password;
+  await act(async () => {
+      page = await render(<Register />);
+      password = await page.getByTestId("register-password")
+  });
+  await act(async () => {
+    await password.focus();
+    expect(password).toHaveValue("");
+    await fireEvent.change(password, { target: { value: '1' } });
+    expect(password).toHaveValue("1");
+    await fireEvent.change(password, { target: { value: '12' } });
+    expect(password).toHaveValue("12");
+    await fireEvent.change(password, { target: { value: 'setare78' } });
+    expect(password).toHaveValue('setare78');
+    await fireEvent.change(password, { target: { value: '' } });
+    expect(password).toHaveValue('');
+  })
 });
