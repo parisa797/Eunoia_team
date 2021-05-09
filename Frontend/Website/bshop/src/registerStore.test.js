69824351002f2cbac10cadce0f7d7a89efb4ad96
fateme jouyande <fj.1403@yahoo.com>
2021-05-10 01:07:40 +0430

    register shop error handeling tests

--- Frontend/Website/bshop/src/registerStore.test.js
+++ Frontend/Website/bshop/src/registerStore.test.js
@@ -3,15 +3,22 @@ import { render, fireEvent } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import RegisterStore from './registerStore';
 import '@testing-library/jest-dom';
+import * as Snackbar from 'notistack';
+import * as api from "./api";
 
+const enqueueSnackbarMock = jest.fn()
 
-
+const closeSnackbarMock = jest.fn()
+const flushPromises = () => new Promise(setImmediate)
 let container = null;
 beforeEach(() => {
+  
+  //mocking snackbar
+  jest.spyOn(Snackbar, 'useSnackbar').mockImplementation(() => ({enqueueSnackbar:enqueueSnackbarMock, closeSnackbar:closeSnackbarMock}))
+
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
-  localStorage.setItem("role","seller");
 });
 
 afterEach(() => {
@@ -22,32 +29,13 @@ afterEach(() => {
 });
 
 test("register for store", async () => {
-
-  jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve("")
-    })
-  );
   var page;
   await act(async () => {
     page = await render(<RegisterStore />);
   });
-
 });
 
 test("register shop storename ", async () => {
-  const Register = {
-    storeName: "هایپراستار",
-    ownerName: "setare",
-    address: "khonamon",
-    code: "123456",
-    phone: "09122111111"
-  };
-  // jest.spyOn(global, "fetch").mockImplementation(() =>
-  //     Promise.resolve({
-  //     json: () => Promise.resolve(RegisterStore)
-  //     })
-  // );
   var page;
   var storeName;
   await act(async () => {
@@ -67,51 +55,27 @@ test("register shop storename ", async () => {
   })
 });
 
-test("register shop username ", async () => {
-  const Register = {
-    storeName: "هایپراستار",
-    ownerName: "setare",
-    address: "khonamon",
-    code: "123456",
-    phone: "09122111111"
-  };
-  // jest.spyOn(global, "fetch").mockImplementation(() =>
-  //     Promise.resolve({
-  //     json: () => Promise.resolve(RegisterStore)
-  //     })
-  // );
+test("register shop storename ", async () => {
   var page;
-  var username;
+  var storename;
   await act(async () => {
       page = await render(<RegisterStore />);
-      username = await page.getByTestId("register-shop-ownername");
+      storename = await page.getByTestId("register-shop-name");
   });
 
   await act(async () => {
-    await username.focus();
-    expect(username).toHaveValue("");
-    await fireEvent.change(username, { target: { value: 'setare' } });
-    expect(username).toHaveValue("setare");
-    await fireEvent.change(username, { target: { value: 'ستاره' } });
-    expect(username).toHaveValue('ستاره');
-    await fireEvent.change(username, { target: { value: '' } });
-    expect(username).toHaveValue('');
+    await storename.focus();
+    expect(storename).toHaveValue("");
+    await fireEvent.change(storename, { target: { value: 'setare' } });
+    expect(storename).toHaveValue("setare");
+    await fireEvent.change(storename, { target: { value: 'ستاره' } });
+    expect(storename).toHaveValue('ستاره');
+    await fireEvent.change(storename, { target: { value: '' } });
+    expect(storename).toHaveValue('');
   })
 });
 
 test("register shop phone ", async () => {
-  const Register = {
-    storeName: "هایپراستار",
-    ownerName: "setare",
-    address: "khonamon",
-    code: "123456",
-    phone: "09122111111"
-  };
-  // jest.spyOn(global, "fetch").mockImplementation(() =>
-  //     Promise.resolve({
-  //     json: () => Promise.resolve(RegisterStore)
-  //     })
-  // );
   var page;
   var phone;
   await act(async () => {
@@ -133,18 +97,6 @@ test("register shop phone ", async () => {
 
 
 test("register shop code ", async () => {
-  const Register = {
-    storeName: "هایپراستار",
-    ownerName: "setare",
-    address: "khonamon",
-    code: "123456",
-    phone: "09122111111"
-  };
-  // jest.spyOn(global, "fetch").mockImplementation(() =>
-  //     Promise.resolve({
-  //     json: () => Promise.resolve(RegisterStore)
-  //     })
-  // );
   var page;
   var code;
   await act(async () => {
@@ -162,3 +114,99 @@ test("register shop code ", async () => {
   })
 });
 
+test("register shop region ", async () => {
+  var page;
+  var region;
+  await act(async () => {
+      page = await render(<RegisterStore />);
+      region = await page.getByTestId("register-shop-region");
+  });
+  await act(async () => {
+    // await region.focus();
+    // expect(region).toHaveValue("");
+    await fireEvent.change(region, { target: { value: '22' } });
+    expect(region).toHaveValue("22");
+    await fireEvent.change(region, { target: { value: '' } });
+    expect(region).toHaveValue('');
+  })
+});
+
+test("register shop address length ", async () => {
+  var page;
+  var address;
+  var btn;
+  await act(async () => {
+      page = await render(<RegisterStore />);
+      address = await page.getByTestId("register-shop-address");
+      btn = await page.getByTestId("register-shop-button");
+  });
+
+  await act(async () => {
+    await fireEvent.change(address, { target: { value: 'a' } });
+    expect(address).toHaveValue("a");
+    await btn.click();
+    expect(enqueueSnackbarMock).toBeCalledTimes(1);
+    expect(enqueueSnackbarMock).toHaveBeenLastCalledWith('در پر کردن اطلاعات دقت بیشتری لحاظ نمایید.', { variant: 'error',})
+
+  })
+});
+
+test("register shop ownerName length ", async () => {
+  var page;
+  var ownerName;
+  var btn;
+  await act(async () => {
+      page = await render(<RegisterStore />);
+      ownerName = await page.getByTestId("register-shop-ownername");
+      btn = await page.getByTestId("register-shop-button");
+  });
+
+  await act(async () => {
+    await fireEvent.change(ownerName, { target: { value: 'a' } });
+    expect(ownerName).toHaveValue("a");
+    await btn.click();
+    expect(enqueueSnackbarMock).toBeCalledTimes(1);
+    expect(enqueueSnackbarMock).toHaveBeenLastCalledWith('در پر کردن اطلاعات دقت بیشتری لحاظ نمایید.', { variant: 'error',})
+
+  })
+});
+
+test("register shop storeName length ", async () => {
+  var page;
+  var storeName;
+  var btn;
+  await act(async () => {
+      page = await render(<RegisterStore />);
+      storeName = await page.getByTestId("register-shop-name");
+      btn = await page.getByTestId("register-shop-button");
+  });
+
+  await act(async () => {
+    await fireEvent.change(storeName, { target: { value: 'a' } });
+    expect(storeName).toHaveValue("a");
+    await btn.click();
+    expect(enqueueSnackbarMock).toBeCalledTimes(1);
+    expect(enqueueSnackbarMock).toHaveBeenLastCalledWith('در پر کردن اطلاعات دقت بیشتری لحاظ نمایید.', { variant: 'error',})
+
+  })
+});
+
+test("register shop code length ", async () => {
+  var page;
+  var code;
+  var btn;
+  await act(async () => {
+      page = await render(<RegisterStore />);
+      code = await page.getByTestId("register-shop-code");
+      btn = await page.getByTestId("register-shop-button");
+  });
+
+  await act(async () => {
+    await fireEvent.change(code, { target: { value: 'a' } });
+    expect(code).toHaveValue("a");
+    await btn.click();
+    expect(enqueueSnackbarMock).toBeCalledTimes(1);
+    expect(enqueueSnackbarMock).toHaveBeenLastCalledWith('در پر کردن اطلاعات دقت بیشتری لحاظ نمایید.', { variant: 'error',})
+
+  })
+});
