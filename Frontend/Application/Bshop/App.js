11f5d9ad7871189413b3c5a62cf7fa833a5247a5
ghzamani <ghzamaninejad@gmail.com>
2021-05-19 21:30:12 +0430

    Add sidebar

--- Frontend/Application/Bshop/App.js
+++ Frontend/Application/Bshop/App.js
@@ -1,56 +1,129 @@
-// import { StatusBar } from "expo-status-bar";
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
-// import Navigations from "./app/navigation";
-
-// export default function App() {
-//   return (
-//     // <View style={styles.container}>
-//     //   <Text>Open up App.js to start working on your app!</Text>
-//     //   <StatusBar style="auto" />
-//     // </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { DrawerActions } from "react-navigation";
+import * as SecureStore from "expo-secure-store";
 
 import Welcome from "./app/welcome";
 import Login from "./app/login";
 import SignUp from "./app/signup";
 import Home from "./app/homepage";
 import ShopDetail from "./app/shopDetails";
+import PersonalInfo from "./app/PersonalInfo";
+// import { AuthStack, AppStack } from "./app/navigation";
 
 // import { SnackbarProvider } from "notistack";
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-export default function App() {
+//Structure for the navigatin Drawer
+const NavigationDrawerStructure = (props) => {
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.openDrawer();
+  };
+
   return (
-    // <SnackbarProvider maxSnack={3}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          // options={{ title: "خوش" }}
-          name="Welcome"
-          component={Welcome}
+    <View style={{ flexDirection: "row" }}>
+      <TouchableOpacity onPress={toggleDrawer}>
+        {/*Donute Button Image */}
+        <Image
+          source={{
+            uri: "https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png",
+          }}
+          style={{ width: 25, height: 25, marginLeft: 15 }}
         />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={SignUp} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ShopDetail" component={ShopDetail} />
-      </Stack.Navigator>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const AppStack = ({ navigation }) => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: " ", //Set Header Title
+          headerLeft: () => (
+            <NavigationDrawerStructure navigationProps={navigation} />
+          ),
+          headerStyle: {
+            backgroundColor: "#780909", //Set Header color
+          },
+          headerTintColor: "#fff", //Set Header text color
+          // headerTitleStyle: {
+          //   fontWeight: "bold", //Set Header text style
+          // },
+        }}
+      />
+      <Stack.Screen
+        name="ShopDetail"
+        component={ShopDetail}
+        options={{ title: "اطلاعات فروشگاه" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="homepage"
+        options={{
+          drawerLabel: "صفحه اصلی",
+          activeTintColor: "#e91e63",
+        }}
+        component={AppStack}
+      />
+      <Drawer.Screen
+        name="profile"
+        options={{
+          drawerLabel: "پروفایل",
+          activeTintColor: "#e91e63",
+        }}
+        component={PersonalInfo}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default function App({ navigation }) {
+  const [loggedin, setlogin] = React.useState(false);
+
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      token = await SecureStore.getItemAsync("token");
+      if (token) setlogin(true);
+    };
+    bootstrapAsync();
+  }, []);
+
+  console.log(loggedin);
+
+  return (
+    //state in not working true
+    <NavigationContainer>
+      {loggedin == false ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={SignUp} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="drawer-stack"
+            component={Sidebar}
+            options={{ title: " " }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
-    // </SnackbarProvider>
   );
 }
--- Frontend/Application/Bshop/app/navigation.js
+++ Frontend/Application/Bshop/app/navigation.js
@@ -1,20 +1,56 @@
 import * as React from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { View, Text } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
 
 import Welcome from "./welcome";
 import Login from "./login";
+import SignUp from "./signup";
+
+import Home from "./homepage";
+import shopDetail from "./shopDetails";
+
+// const Stack = createStackNavigator();
+
+// export default Navigations = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Welcome">
+//         <Stack.Screen name="Welcome" component={Welcome} />
+//         <Stack.Screen name="Login" component={Login} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
-export default Navigations = () => {
+const AppStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="ShopDetail" component={ShopDetail} />
+    </Stack.Navigator>
   );
 };
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+};
+
+// export const Router = () => {
+//   //More explanations about "authData" below
+//   return (
+//     <NavigationContainer>
+//       {authData ? <AppStack /> : <AuthStack />}
+//     </NavigationContainer>
+//   );
+// };
