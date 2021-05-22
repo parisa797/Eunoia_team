3aca6783fb8341d6ffe8e5f5ddc3c61a2bb1a80c
fateme jouyande <fj.1403@yahoo.com>
2021-05-23 00:03:49 +0430

    stars for item

--- Frontend/Website/bshop/src/Item.js
+++ Frontend/Website/bshop/src/Item.js
@@ -1,73 +1,13 @@
-// import React, { useState, useEffect } from "react";
-// import './Item.css'
-// import ShopSideBar from './ShopSideBar';
-// function Item(props) {
-//     // console.log("Token "+localStorage.getItem("token"))
-//     const [items, setItems] = useState({});
-//     var shopID = window.location.pathname.match(/[^\/]+/g)[1]
-//     var itemID = window.location.pathname.match(/[^\/]+/g)[3]
-//     // let itemId = window.location.pathname.match.items(/[^\/]+/g)[3]
-//     // let shopID = window.location.pathname.match(/[^\/]+/g)[1]
-//     useEffect(() => {
-//         fetch("http://eunoia-bshop.ir:8000/shops/"+shopID+"/items/"+itemID, {
-//             method: 'GET',
-//             // headers: {
-//             //     "Authorization": "Token " + localStorage.getItem('token')
-//             // }
-//         }).then((res) => {
-//             if (res.status === 200) {
-//                 return res.json();
-//             }
-//         }).then(res => {
-//             setItems(res);
-//         })
-//     }, [])
-//     useEffect(() => {
-
-//     }, [])
-  
-//     console.log(items);
-//     return (
-//         <div style={{padding: "5vh 2vw" }}>
-//             <ShopSideBar />
-//             <div className="page-contents">
-//         <div className="item-page" >
-            
-//             <div className="sub-container">
-                
-//                 <img src={items.photo} class="w3-hover-sepia" alt=""></img>
-//                 {!!items.manufacture_jalali &&<><div className="manufacture_jalali" data-testid="item-manufacture_jalali">تاریخ تولید : {items.manufacture_jalali}</div></>}
-//                         {!!items.Expiration_jalali&&<><div className="Expiration_jalali"  data-testid="item-Expiration_jalali" >تاریخ انقضا : {items.Expiration_jalali}</div></>}
-                
-//                 <div className="description" data-testid="item" style={{ width: "50%", direction: "rtl", zIndex: "1", margin: "0" }}>
-//                  <div className="column">
-                
-//                     <h1 data-testid="item-name" class="item-title">{items.name}</h1>
-//                     {!!items.description &&<><h3 data-testid="item-description">ویژگی های کالا : </h3>
-//                         <div>{items.description}</div></>}
-                        
-//                         {!!items.count&&<><div data-testid="item-count"> موجودی : {items.count}</div></>}
-//                         {!!items.phone&&<><div data-testid="item-phone">شماره فروشگاه : {items.ItemShop?.phone}</div></>}
-//                         {props.userState !== "m" && <a href="#" className="btn btn-primary" >خرید</a>}
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         </div>
-//         </div>
-
-//     )
-// }
-// export default Item;
-
-// {/* {items.ItemShop && items.ItemShop.phone} */ }
-
 import React, { useState, useEffect } from "react";
 import "./Item.css";
+import ReactStars from "react-rating-stars-component";
 import ShopSideBar from "./ShopSideBar";
 function Item(props) {
   // console.log("Token "+localStorage.getItem("token"))
   const [items, setItems] = useState({});
+  const [itemInfo, setItemInfo] = useState({})
+  const [rated, setRated] = useState(false)
+  const [rateID, setRateId] = useState(null)
   var shopID = window.location.pathname.match(/[^\/]+/g)[1];
   var itemID = window.location.pathname.match(/[^\/]+/g)[3];
   // let itemId = window.location.pathname.match.items(/[^\/]+/g)[3]
@@ -89,6 +29,53 @@ function Item(props) {
       });
   }, []);
   useEffect(() => {}, []);
+  function ratingChanged(new_rating) {
+    let fd = new FormData()
+    console.log(rated)
+    fd.append("rate", new_rating)
+    if (!rated) {
+        fd.append("item", itemID)
+        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/rate/create/", {
+            method: 'POST',
+            headers: {
+                "Authorization": "Token " + localStorage.getItem('token')
+            }
+            ,
+            body: fd
+        }).then(res => {
+            if (res.status === 201) {
+
+                return res.json()
+            }
+            console.log(res.status)
+            return null
+        }).then(res => {
+            if (res) {
+                setRateId(res.id)
+                setRated(true)
+                props.setTriggerReload(!props.triggerReload)
+            }
+
+        })
+    }
+    else {
+        fetch("http://eunoia-bshop.ir:8000/api/v1/items/rate/" + rateID, {
+            method: 'PUT',
+            headers: {
+                "Authorization": "Token " + localStorage.getItem('token')
+            }
+            ,
+            body: fd
+        }).then(res => {
+            if (res.status === 200) {
+                setRated(true)
+                props.setTriggerReload(!props.triggerReload)
+            }
+            console.log(res.status)
+        })
+    }
+
+}
 
   console.log(items);
   return (
@@ -99,7 +86,7 @@ function Item(props) {
           <div className="sub-container">
             <div className="col-12 d-flex flex-wrap">
               <div className="col-sm-12 col-md-5">
-                <img style={{ width: "110%" }} src={items.photo} alt="" />
+                <img style={{ width: "100%", height: "100%" }} src={items.photo} alt="" />
               </div>
               <div className="lead text-right col-sm-12 col-md-6">
                 <h1 className="my-3" data-testid="item-name">{items.name}</h1>
@@ -110,6 +97,42 @@ function Item(props) {
                     <p className="lead text-right" data-testid="item-description">{items.description}</p>
                   </>
                 )}
+                <div className="rating-item">
+                            <> <p style={{ direction: "ltr" }} data-testid={"item-rate-value"}>امتیاز: {itemInfo.rate_value?Math.round(itemInfo.rate_value * 10) / 10 :0} </p>
+                                    {!!itemInfo.rate_value && <ReactStars
+                                        edit={props.userState!=="u"}
+                                        value={Math.round(itemInfo.rate_value)}
+                                        isHalf={false}
+                                        classNames="stars"
+                                        data-testid="item-rate-stars"
+                                        size={25}
+                                        onChange={ratingChanged}
+                                        activeColor={"var(--primary-color)"}
+                                    />}
+                                    {!itemInfo.rate_value && <ReactStars
+                                        edit={!!localStorage.getItem("token")}
+                                        value={0}
+                                        isHalf={false}
+                                        classNames="stars"
+                                        data-testid="shop-rate-stars"
+                                        size={25}
+                                        onChange={ratingChanged}
+                                        activeColor={"var(--primary-color)"}
+                                    /> }
+                                    <p data-testid={"item-rate-count"}>‌({itemInfo.rate_count})</p>
+                                </>
+                            </div>
+                 {/* <div className="items-stars">
+                                    <ReactStars
+                                        edit={false}
+                                        value={items.rate_value ? items.rate_value : 0}
+                                        isHalf={true}
+                                        classNames="stars"
+                                        size={20}
+                                        activeColor={"var(--primary-color)"}
+                                    />
+                                    <p data-testid={"region-items-rate-count" + items.id}>({items.rate_count})</p>
+                                </div> */}
                 {!!items.count && (
                   <div className="col-12 d-flex flex-justify-between p-0 my-2">
                     <div className="col-6 p-0 text-right">موجودی:</div>
--- Frontend/Website/bshop/src/ItemsList.js
+++ Frontend/Website/bshop/src/ItemsList.js
@@ -1,64 +1,4 @@
 
-// import { useEffect ,useState } from "react";
-// import "./ItemsList.css";
-
-// function Itemslist(props){
-//     const [items,setItems] = useState([]);
-//     useEffect(()=>{
-//         fetch("http://eunoia-bshop.ir:8000/shops/1/items/",{
-//         method: "GET",
-//         headers: {
-//             "Authorization" : "Token " + localStorage.getItem("token")
-//         }
-//         }).then((res)=>{
-//             if(res.status === 200){
-//                 return res.json();
-//             }
-//         }).then(res =>{
-//             res=[{name:"پنیر", description:"پنیر خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3, price:2000 , photo:"https://media.mehrnews.com/d/2020/06/05/3/3468748.jpg"}
-//         ,{name:"کره", description:"کرههههههههه خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-01-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://cdn.tezolmarket.com/media/products/large/1361405_191.jpg"},
-//         {name:"شییییر دامداران", description:"شیر دامداران بهترین است. حرف ندارد", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://rahak.com/wp-content/uploads/2018/12/%D8%B4%DB%8C%D8%B1-%DA%A9%D8%A7%D9%85%D9%84-%D8%AF%D8%A7%D9%85%D8%AF%D8%A7%D8%B1%D8%A7%D9%86.jpg"},
-//         {name:"پنیر", description:"پنیر خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://media.mehrnews.com/d/2020/06/05/3/3468748.jpg"}
-//         ,{name:"کره", description:"کرههههههههه خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-01-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://cdn.tezolmarket.com/media/products/large/1361405_191.jpg"},
-//         {name:"شییییر دامداران", description:"شیر دامداران بهترین است. حرف ندارد", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://rahak.com/wp-content/uploads/2018/12/%D8%B4%DB%8C%D8%B1-%DA%A9%D8%A7%D9%85%D9%84-%D8%AF%D8%A7%D9%85%D8%AF%D8%A7%D8%B1%D8%A7%D9%86.jpg"},
-//         {name:"پنیر", description:"پنیر خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3, price:2000 , photo:"https://media.mehrnews.com/d/2020/06/05/3/3468748.jpg"}
-//         ,{name:"کره", description:"کرههههههههه خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-01-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://cdn.tezolmarket.com/media/products/large/1361405_191.jpg"},
-//         {name:"شییییر دامداران", description:"شیر دامداران بهترین است. حرف ندارد", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://rahak.com/wp-content/uploads/2018/12/%D8%B4%DB%8C%D8%B1-%DA%A9%D8%A7%D9%85%D9%84-%D8%AF%D8%A7%D9%85%D8%AF%D8%A7%D8%B1%D8%A7%D9%86.jpg"},
-//         {name:"پنیر", description:"پنیر خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3, price:2000 , photo:"https://media.mehrnews.com/d/2020/06/05/3/3468748.jpg"}
-//         ,{name:"کره", description:"کرههههههههه خیلی خوبه برا بدن مفیده ولی نه زیادیش.", manufacture_Date:"2000-01-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://cdn.tezolmarket.com/media/products/large/1361405_191.jpg"},
-//         {name:"شییییر دامداران", description:"شیر دامداران بهترین است. حرف ندارد", manufacture_Date:"2000-02-01",Expiration_Date:"2000-03-01",count:3 ,price:2000, photo:"https://rahak.com/wp-content/uploads/2018/12/%D8%B4%DB%8C%D8%B1-%DA%A9%D8%A7%D9%85%D9%84-%D8%AF%D8%A7%D9%85%D8%AF%D8%A7%D8%B1%D8%A7%D9%86.jpg"},
-//     ]
-//             setItems(res);
-//         })
-//     },[])
-
-//     return(
-//        // <div className="Items_list">
-//         <div className="Items_list container-fluid justify-content-center row your-shops" data-testid="myitems" style={{ width: "100%", zIndex: "0", margin: "0 0 20vh 0", padding: "10" }}  >    
-//         {items.map((item)=>{
-//             if(!!item) return(
-//                // <div className="item-outer-container col-12 col-sm-6 col-md-4 col-xl-3" key={item.name} data-testid={"firstItem" + item.name}>
-//                 <div class="card col-12 col-sm-6 col-md-4 col-lg-3">
-//                 <div class="card-body">
-//                 <h5 class="btn btn-primary" style={{backgroundColor :"var(--secondary-color)",width: "100%" }}>{item.name} </h5>
-
-//                 {item.photo && /*<div className="container-fluid justify-content-center row your-shops">*/<img data-testid={"myitemس-img-"} src={item.photo} />/*</div>*/}
-//                 <p class="card-text">موجودی  :{item.count} عدد </p>
-//                 <p class="card-text"> قیمت: {item.price} تومان </p>
-//                 {/* <p class="card-text">  توضیحات</p>
-//                 <p class = "card_description">{item.description}</p> */}
-
-//                 <a href="#" class="btn btn-primary" style={{backgroundColor :"var(--primary-color)" }} >مشاهده محصول</a>
-//                 </div>
-//                 </div>
-//             )
-//             })}
-//         </div>
-//     )
-// }
-// export default Itemslist;
-
-
 import { useEffect, useState } from "react";
 import "./ItemsList.css";
 import ItemCard from "./ItemCard";
@@ -110,13 +50,6 @@ function Itemslist(props) {
                         if (!!item) return (
                             <div /*class="card col-12 col-sm-6 col-md-4 col-lg-3"*/ className="col-12 col-sm-6 col-md-4 col-lg-3" style={{ padding: "5px" }}>
                                 <ItemCard item={item} id={props.id} onlineShop={shopInfo.online} showDeleteItemModal={props.showDeleteItemModal} userState={props.userState}/>
-                                {/* <div class="card-body">
-                <h5 class="btn btn-primary" style={{backgroundColor :"var(--secondary-color)",width: "100%" }}>{item.name} </h5>
-                {item.photo && <img data-testid={"myitem-img-"} src={item.photo} />}
-                <p class="card-text">موجودی  :{item.count} عدد </p>
-                <p class="card-text"> قیمت: {item.price} تومان </p>
-                <a href="#" class="btn btn-primary" style={{backgroundColor :"var(--primary-color)" }} >مشاهده محصول</a>
-                </div> */}
                             </div>
                         )
                     })}
