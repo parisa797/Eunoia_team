2841adf0a4c36600e3b9b8e6c451f9377bbf1b0e
parisa797 <73137848+parisa797@users.noreply.github.com>
2021-05-22 19:46:01 +0430

    add ui shoplist and like shop

--- Frontend/Application/Bshop/app/shop.js
+++ Frontend/Application/Bshop/app/shop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,46 +7,45 @@ import {
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform,
+  Pressable,
 } from "react-native";
 import StarRating from "react-native-star-rating";
 import Icon from "react-native-vector-icons/FontAwesome";
+import AntIcon from "react-native-vector-icons/AntDesign";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Shop = (props) => {
+  const [liked, setLiked] = useState(false);
   const shop_name = props.title.includes("فروشگاه")
     ? props.title
     : "فروشگاه " + props.title;
-  const shop_add = "آدرس: " + props.address;
+
   return (
     <View style={styles.shop}>
-      <TouchableOpacity
-        testID={"shop-" + props.index}
-        onPress={props.onSelect}
-        useForeground
-      >
+      <Pressable onPress={() => setLiked((isLiked) => !isLiked)}>
+        <MaterialCommunityIcons
+          name={liked ? "heart" : "heart-outline"}
+          size={32}
+          color={liked ? "red" : "black"}
+        />
+      </Pressable>
+      <TouchableOpacity onPress={props.onSelect} useForeground>
         <View style={styles.name_logo}>
           <View style={styles.imageContainer}>
-            <Image
-              testID={"shop-image-" + props.index}
-              style={styles.image}
-              source={{ uri: props.image }}
-            />
+            <Image style={styles.image} source={{ uri: props.image }} />
           </View>
           <View style={styles.name_star}>
-            <Text testID={"shop-name-" + props.index} style={styles.title}>
-              {shop_name}
-            </Text>
+            <Text style={styles.title}>{shop_name}</Text>
             <StarRating
               starSize={25}
               disabled={true}
               fullStarColor={"#b31414"}
-              rating={props.rate_value == 0 ? 0 : props.rate_value}
+              rating={props.rate_value == 0 ? 3 : props.rate_value}
             ></StarRating>
           </View>
         </View>
         <View style={styles.details}>
-          <Text testID={"shop-add-" + props.index} style={styles.address}>
-            {shop_add}
-          </Text>
+          <Text style={styles.address}>آدرس: {props.address}</Text>
           {props.online == true && (
             <View style={styles.online_icon}>
               <Icon name="check-circle" size={20} color="green"></Icon>
@@ -67,7 +66,8 @@ const Shop = (props) => {
 
 const styles = StyleSheet.create({
   shop: {
-    height: 220,
+    height: 260,
+    width: 360,
     margin: 20,
     borderRadius: 20,
     shadowColor: "black",
@@ -75,6 +75,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     elevation: 5,
     backgroundColor: "#f1f1f2",
+    marginLeft: 17,
+
     //right to left
     // justifyContent: "space-between",
     // flexDirection: "row-reverse",
@@ -85,15 +87,18 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     flexDirection: "row-reverse",
     borderRadius: 20,
+    marginLeft: 6,
+    marginTop: -2,
   },
 
   imageContainer: {
     backgroundColor: "white",
     // objectFit: ""
     width: "40%",
-    height: "70%",
+    height: "75%",
     alignSelf: "center",
-    borderRadius: 10,
+    borderRadius: 20,
+    marginTop: -20,
   },
   image: {
     // backgroundColor: "#f1f1f2",
@@ -104,24 +109,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     // paddingBottom: "1",
-    borderRadius: 10,
   },
   name_star: {
     justifyContent: "center",
     // flexGrow: 0.1,
     flexShrink: 0.5,
+    marginTop: -18,
   },
   details: {
     alignItems: "center",
     height: "10%",
-    padding: 10,
+    padding: 30,
+    marginTop: -18,
   },
   title: {
     // fontFamily: "open-sans-bold",
-    fontSize: 25,
+    fontSize: 20,
     marginVertical: 2,
     alignSelf: "center",
     // paddingTop: 3,
+    // marginTop: "0%",
   },
   online_icon: {
     justifyContent: "space-between",
@@ -129,6 +136,14 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "baseline",
   },
+  // heart: {
+  //   justifyContent: "space-between",
+  //   flexDirection: "row-reverse",
+  //   backgroundColor: "transparent",
+  //   alignItems: "baseline",
+  //   marginTop: -190,
+  //   marginLeft: 280,
+  // },
   address: {
     // fontFamily: "open-sans",
     fontSize: 21,
--- Frontend/Application/Bshop/app/shopDetails.js
+++ Frontend/Application/Bshop/app/shopDetails.js
@@ -1,23 +1,288 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   Image,
   StyleSheet,
+  ImageBackground,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform,
+  FlatList,
+  Pressable,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import StarRating from "react-native-star-rating";
 
+// // export default class ShopDetail extends React.Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //       exampleData: [
+// //         {const Product_kind_one = [{
+// //     name : 'Nice cloth',
+// //     price : '2,999',
+// //     image : ('../imgs/1.jpg')
+// // },{
+// //     name : 'Orange cloth',
+// //     price : '3,999',
+// //     image : ('../imgs/2.jpg')
+// // },},
+// //         { name: "Big Watch", price: "888" },
+// //         { name: "Expensive Watch", price: "999999" },
+// //         { name: "Donald Trump", price: "-1" },
+// //       ],
+// //     };
+// //   }
+// //   render() {
+
+// // const ShopDetail = ({ navigation }) => {
+// //   const ShopDetail = [
+// //     {
+// //       name: "Nice cloth",
+// //       price: "2,999",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Orange cloth",
+// //       price: "3,999",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Pink cloth",
+// //       price: "2,999",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Colory cloth",
+// //       price: "1,999",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Dark High Heels",
+// //       price: "0,999",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Blue Nice Shoes",
+// //       price: "3,599",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //     {
+// //       name: "Women Blue Bag",
+// //       price: "2,299",
+// //       image: ("../assets/lemon.jpg"),
+// //     },
+// //   ];
+
+// //   return (
+// //     <View>
+// //       <Text style={{ fontWeight: "bold" }}>محصولات این فروشگاه</Text>
+// //       <FlatList
+// //         horizontal
+// //         data={Product_kind_one}
+// //         renderItem={({ item }) => (
+// //           <View style={{ padding: 10 }}>
+// //             <View
+// //               style={{
+// //                 backgroundColor: "white",
+// //                 borderWidth: 0,
+// //                 borderColor: "#626262",
+// //                 borderRadius: 20,
+// //                 height: 200,
+// //                 width: 150,
+// //               }}
+// //             />
+// //             <Text>{item.name}</Text>
+// //             <Text>{item.price}</Text>
+// //           </View>
+// //         )}
+// //       />
+// //     </View>
+// //   );
+// // };
+
+// // export default ShopDetail;
+
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         source={("../assets/lemon.jpg")}
+//         style={styles.imagebackk}
+//       >
+//         <View style={styles.product}>
+//           {/* <View style={styles.touchable}> */}
+//           {/* <TouchableOpacity onPress={props.onSelect} useForeground> */}
+//           <View>
+//             <View style={styles.imageContainer}>
+//               <Image style={styles.image} source={{ uri: route.params.logo }} />
+//             </View>
+//             <View style={styles.details}>
+//               <Text style={styles.title}>فروشگاه {route.params.title}</Text>
+//               <Text style={styles.address}>
+//                 واقع شده در منطقه {route.params.mantaghe}
+//               </Text>
+//               <Text style={styles.address}>
+//                 با مدیریت {route.params.manager}
+//               </Text>
+//               <Text style={styles.address}>آدرس: {route.params.address}</Text>
+//               {route.params.online == true && (
+//                 <View style={styles.online_icon}>
+//                   <Icon name="check-circle" size={20} color="green"></Icon>
+//                   <Text style={{ fontSize: 20 }}> فروش آنلاین دارد</Text>
+//                 </View>
+//               )}
+//               {route.params.online == false && (
+//                 <View style={styles.online_icon}>
+//                   <Icon name="times-circle" size={20} color="red"></Icon>
+//                   <Text style={{ fontSize: 20 }}> فروش آنلاین ندارد</Text>
+//                 </View>
+//               )}
+//               <View>
+//                 <StarRating
+//                   starSize={25}
+//                   disabled={true}
+//                   fullStarColor={"yellow"}
+//                   rating={
+//                     route.params.rate_value == 0 ? 3 : route.params.rate_value
+//                   }
+//                 ></StarRating>
+//               </View>
+//             </View>
+//             {/* <View style={styles.actions}>{props.children}</View> */}
+//           </View>
+//           {/* </TouchableOpacity> */}
+//           {/* </View> */}
+//         </View>
+//       </ImageBackground>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   product: {
+//     height: 200,
+//     margin: 20,
+//     borderRadius: 20,
+//   },
+
+//   imageContainer: {
+//     backgroundColor: "white",
+//     // objectFit: ""
+//     width: "55%",
+//     height: "70%",
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//     // alignContent: "center",
+//     alignSelf: "center",
+
+//     // borderTopLeftRadius: 10,
+//     // borderTopRightRadius: 10,
+//     borderRadius: 10,
+//     marginTop: 70,
+
+//     // overflow: "hidden",
+//   },
+//   image: {
+//     // backgroundColor: "#f1f1f2",
+//     width: "100%",
+//     height: "100%",
+//     marginTop: "1%",
+//     resizeMode: "cover",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginTop: 10,
+
+//     // paddingBottom: "1",
+//   },
+//   details: {
+//     alignItems: "center",
+//     height: "17%",
+//     padding: 10,
+//     marginTop: 10,
+//   },
+//   title: {
+//     // fontFamily: "open-sans-bold",
+//     fontSize: 40,
+//     marginVertical: 10,
+//     // paddingTop: 3,
+//   },
+//   online_icon: {
+//     justifyContent: "space-between",
+//     flexDirection: "row-reverse",
+//     backgroundColor: "#f1f1f2",
+//     alignItems: "baseline",
+//   },
+//   address: {
+//     // fontFamily: "open-sans",
+//     fontSize: 20,
+//     marginVertical: 5,
+//     color: "black",
+//   },
+//   container: {
+//     flex: 1,
+//     // width: "100%",
+//     // height: "100%",
+//     flexDirection: "column",
+//   },
+//   imagebackk: {
+//     width: 400,
+//     height: 700,
+//     marginTop: -20,
+//     marginLeft: -7,
+//   },
+// });
+
 const ShopDetail = ({ route, navigation }) => {
   console.log(route.params);
+
+  const shopitems = [
+    {
+      name: "پفک طلایی چی توز",
+      price: "7.000",
+      image: require("../assets/pofak.png"),
+      id: 1,
+    },
+    {
+      name: "پاستیل شیبابا",
+      price: "9.000",
+      image: require("../assets/pastil.jpg"),
+      id: 2,
+    },
+    {
+      name: "رب گوجه آتا",
+      price: "15.100",
+      image: require("../assets/rob.png"),
+      id: 3,
+    },
+    {
+      name: "پف پفی شیبابا",
+      price: "7.000",
+      image: require("../assets/pufpuf.jpg"),
+      id: 4,
+    },
+    {
+      name: "پودینگ کوپا",
+      price: "10.500",
+      image: require("../assets/pudding.png"),
+      id: 5,
+    },
+    {
+      name: "چیپس قارچ و خامه چی توز",
+      price: "8.000",
+      image: require("../assets/chips.png"),
+      id: 6,
+    },
+    {
+      name: "پودر شکلات پارمیدا",
+      price: "33.800",
+      image: require("../assets/pudrchocolate.jpg"),
+      id: 7,
+    },
+  ];
   return (
-    <View style={styles.product}>
-      {/* <View style={styles.touchable}> */}
-      {/* <TouchableOpacity onPress={props.onSelect} useForeground> */}
-      <View>
+    <View>
+      <View style={styles.shop}>
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={{ uri: route.params.logo }} />
         </View>
@@ -51,46 +316,110 @@ const ShopDetail = ({ route, navigation }) => {
             ></StarRating>
           </View>
         </View>
-        {/* <View style={styles.actions}>{props.children}</View> */}
       </View>
-      {/* </TouchableOpacity> */}
-      {/* </View> */}
+      <View style={styles.rows}>
+        <Text style={styles.rowstext}>محصولات فروشگاه</Text>
+      </View>
+      <Text style={{ fontWeight: "bold" }}></Text>
+      <FlatList
+        style={{ marginTop: -30 }}
+        horizontal
+        data={shopitems}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          console.log(item.image);
+          console.log("item.image");
+          return (
+            <View style={{ padding: 10 }}>
+              <View style={styles.items}>
+                <Image style={styles.image} source={item.image} />
+              </View>
+              <Text>{item.name}</Text>
+              <Text>{item.price}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  product: {
-    height: 200,
-    margin: 20,
+  shop: {
     borderRadius: 20,
+    marginTop: 10,
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+    backgroundColor: "white",
+    height: 250,
+    width: 370,
+    marginLeft: 10,
   },
-
+  rows: {
+    borderRadius: 20,
+    marginTop: 10,
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+    backgroundColor: "#b31414",
+    height: 50,
+    width: 370,
+    marginLeft: 12,
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+  rowstext: {
+    color: "#fff",
+    fontSize: 22,
+  },
+  items: {
+    backgroundColor: "white",
+    height: 200,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 10,
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+    backgroundColor: "white",
+  },
   imageContainer: {
     backgroundColor: "white",
     // objectFit: ""
-    width: "50%",
-    height: "70%",
+    width: "30%",
+    height: "40%",
     // alignItems: "center",
     // justifyContent: "center",
     // alignContent: "center",
-    alignSelf: "center",
-
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
     // borderTopLeftRadius: 10,
     // borderTopRightRadius: 10,
     borderRadius: 10,
+    marginLeft: 245,
+    marginTop: 10,
 
     // overflow: "hidden",
   },
   image: {
     // backgroundColor: "#f1f1f2",
-    width: "100%",
-    height: "100%",
-    marginTop: "1%",
+    width: "80%",
+    height: "80%",
+    marginTop: "12%",
     resizeMode: "cover",
     alignItems: "center",
     justifyContent: "center",
     // paddingBottom: "1",
+    marginLeft: 12,
   },
   details: {
     alignItems: "center",
@@ -99,14 +428,16 @@ const styles = StyleSheet.create({
   },
   title: {
     // fontFamily: "open-sans-bold",
-    fontSize: 25,
-    marginVertical: 2,
+    fontSize: 19,
+    marginVertical: 65,
     // paddingTop: 3,
+    marginLeft: "-35%",
+    marginTop: "-30%",
   },
   online_icon: {
     justifyContent: "space-between",
     flexDirection: "row-reverse",
-    backgroundColor: "#f1f1f2",
+    backgroundColor: "transparent",
     alignItems: "baseline",
   },
   address: {
