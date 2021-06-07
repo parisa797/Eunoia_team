cd65eabd6cde4ae2513a9f42f59dfacaf8a26cfd
ShMob <mobashers313@gmail.com>
2021-06-07 22:21:40 +0430

    FIX - board api, carousel disappearing after removing an item (not fixed yet)

--- Frontend/Website/bshop/src/ShopBoard.js
+++ Frontend/Website/bshop/src/ShopBoard.js
@@ -7,7 +7,12 @@ import { Modal } from "react-bootstrap";
 function ShopBoard(props) {
     const [board, setBoard] = useState([{ image: "/special-offer.jpg", image_url: "/something" }, { image: "/پیشنهاد-ویزه-وجین-Copy.jpg" }])
     const [showEdit, setShowEdit] = useState(false);
-    const [reloadBoard, setReloadBoard] = useState(false);
+    const [reloadBoard, setReloadBoard] = useState({ state: false });
+    const [loadCarousel, setLoadCarousel ] = useState(true);
+
+    function timeout(delay) {
+        return new Promise(res => setTimeout(res, delay));
+    }
 
     useEffect(() => {
         fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/list/" + props.shopID, {
@@ -17,7 +22,6 @@ function ShopBoard(props) {
             // }
         }).then(res => {
             console.log(res.status)
-
             if (res.ok)
                 return res.json()
         }).then(res => {
@@ -25,6 +29,15 @@ function ShopBoard(props) {
             setBoard(res)
         }).catch(err => console.log(err))
     }, [reloadBoard])
+
+    useEffect(() => {
+        if (!!reloadBoard.page)
+        {
+            setLoadCarousel(false);
+            timeout(100);
+            setLoadCarousel(true);
+        }
+    }, [board])
 
     function addToBoard(e) {
         let files = e.target.files;
@@ -39,45 +52,77 @@ function ShopBoard(props) {
                 method: 'POST',
                 headers: {
                     "Authorization": "Token " + localStorage.getItem('token')
-                }
+                },
+                body: fd
             }).then(res => {
                 console.log(res.status)
                 console.log(res)
-                if (!res.ok)
-                    i--;
+                if (res.ok && i === files.length - 1) {
+                    // const tempboard = [...board]; // Spread syntax creates a shallow copy
+                    // tempboard.push(res); // Spread again to push each selected file individually
+                    // setBoard(tempboard)
+                    setReloadBoard({ state: !reloadBoard.state, page: board.length })
+                }
             }).catch(err => console.log(err))
         }
-        setReloadBoard(!reloadBoard)
     }
 
-    function changeURL(id,elementID) {
+    function changeURL(id, elementID) {
         let url = document.getElementById(elementID).value;
         console.log(url)
-
         let fd = new FormData();
         fd.append("image_url", url);
-        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/update/"+id, {
+        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/update/" + id, {
             method: 'PUT',
             headers: {
                 "Authorization": "Token " + localStorage.getItem('token')
-            }
+            },
+            body: fd
         }).then(res => {
             if (res.ok)
-                setReloadBoard(!reloadBoard)
+                setReloadBoard({ state: !reloadBoard.state })
         }).catch(err => console.log(err))
     }
 
-    function deleteBoard(id) {
-        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/delete/"+id, {
+    function deleteBoard(id, i) {
+        // console.log()
+        console.log(id)
+        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/delete/" + id, {
             method: 'DELETE',
             headers: {
                 "Authorization": "Token " + localStorage.getItem('token')
             }
         }).then(res => {
-            if (res.ok)
-                setReloadBoard(!reloadBoard)
+            console.log(res)
+            if (res.ok) {
+                // if(i === board.length - 1)
+                    // slideToPage(board.length-2);
+                setReloadBoard({ state: !reloadBoard.state, page: board.length-2 })
+            }
         }).catch(err => console.log(err))
     }
+
+    // function slideToPage(num) {
+    //     var dropdowns = document.getElementsByClassName("carousel-item");
+    //     console.log(dropdowns)
+    //     var i;
+    //     for (i = 0; i < dropdowns.length; i++) {
+    //         var openDropdown = dropdowns[i];
+    //         console.log(openDropdown.id)
+    //         if (openDropdown.id === "item-" + num) {
+    //             if (!openDropdown.classList.contains('active'))
+    //                 // openDropdown.classList.push('active');
+    //                 openDropdown.className += " active"
+    //         }
+    //         else if (openDropdown.classList.contains('active')) {
+    //             openDropdown.classList.remove('active');
+    //         }
+    //     }
+    //     // var selectedPages = dropdowns.filter(d=>d.id === num).forEach(element => {
+    //     //     element.classList.push('active');
+    //     // });
+    //     console.log(dropdowns)
+    // }
 
     function changeImage(e, id) {
         let files = e.target.files;
@@ -85,14 +130,15 @@ function ShopBoard(props) {
             return;
         let fd = new FormData();
         fd.append("image", files[0]);
-        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/update/"+id, {
+        fetch("http://eunoia-bshop.ir:8000/api/v1/shops/board/update/" + id, {
             method: 'PUT',
             headers: {
                 "Authorization": "Token " + localStorage.getItem('token')
-            }
+            },
+            body: fd
         }).then(res => {
             if (res.ok)
-                setReloadBoard(!reloadBoard)
+                setReloadBoard({ state: !reloadBoard.state })
         }).catch(err => console.log(err))
     }
 
@@ -101,7 +147,7 @@ function ShopBoard(props) {
             <Carousel interval={null} className="carousel">
                 {board.map((item, i) => {
                     if (item)
-                        return (<Carousel.Item key={i} className="board-item" onClick={() => window.location.href = item.image_url}>
+                        return (<Carousel.Item id={"item-" + i} key={i} className="board-item" onClick={() => window.location.href = item.image_url}>
                             <div className="img-container">
                                 <img src={item.image} alt="board item" />
                             </div>
@@ -120,13 +166,14 @@ function ShopBoard(props) {
                 </Modal.Header>
                 <Modal.Body className="board-modal">
                     <div style={{ direction: "rtl" }}>
-                        <Carousel interval={null} className="carousel">
+                        {loadCarousel && <Carousel interval={null} className="carousel" id="carousel">
+                            {/* <div data-target="#carousel" data-slide-to="next" style={{display:"none"}} id="carousel-page-changer" ></div> */}
                             {!board || board.length == 0 ?
                                 <h4>اعلانی وجود ندارد:(</h4>
                                 :
                                 board.map((item, i) => {
                                     if (item)
-                                        return (<Carousel.Item key={i} className="board-item">
+                                        return (<Carousel.Item id={"item-" + i} key={i} className="board-item">
                                             <div className="board-item-holder">
                                                 <div className="img-container">
                                                     <img src={item.image} alt="board item" />
@@ -134,16 +181,16 @@ function ShopBoard(props) {
                                                 <input type="file" id={"board-photo-" + i} accept="image/*" onChange={e => changeImage(e, item.id)} style={{ display: "none" }} />
                                                 <form className="url-input input-group input-group-lg">
                                                     <input type="text" className="input-lg" id={"board-url-" + i} defaultValue={item.image_url} placeholder="آدرس مربوط به این اعلان را بنویسید" />
-                                                    <div classname="board-btns" style={{ width: "fit-content", margin: "0 auto", direction:"rtl" }}>
-                                                        <div className="btn" onClick={()=>changeURL(item.id,"board-url-" + i)}>{item?.image_url ? "تغییر آدرس" : "اضافه کردن آدرس"}</div>
+                                                    <div className="board-btns" style={{ width: "fit-content", margin: "0 auto", direction: "rtl" }}>
+                                                        <div className="btn" onClick={() => changeURL(item.id, "board-url-" + i)}>{item?.image_url ? "تغییر آدرس" : "اضافه کردن آدرس"}</div>
                                                         <label className="btn board-edit-img" htmlFor={"board-photo-" + i}>تغییر عکس</label>
-                                                        <btn className="btn board-delete" onClick={()=>deleteBoard(item.id)}>حذف اعلان</btn>
+                                                        <div className="btn board-delete" onClick={() => deleteBoard(item.id, i)}>حذف اعلان</div>
                                                     </div>
                                                 </form>
                                             </div>
                                         </Carousel.Item>)
                                 })}
-                        </Carousel>
+                        </Carousel>}
 
                     </div>
                 </Modal.Body>
